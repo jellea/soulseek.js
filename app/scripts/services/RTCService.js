7b39f9301fa42dafd16835b9ebe4741e0dc17dcc
Jelle Akkerman <jelle@jelleakkerman.nl>
2013-05-18 20:03:01 +0400

    big refactor in DataConnection setup

--- app/scripts/services/RTCService.js
+++ app/scripts/services/RTCService.js
@@ -3,125 +3,113 @@
 angular.module('p2pmusicApp')
   .factory('RTCService', function ($rootScope, fileService) {
 
-    window.mainChannel = new DataChannel();
-
-    var URL = "http://localhost:9002/"
-
-    mainChannel.openSignalingChannel = function (config) {
-        var _channel = config.channel || this.channel || 'default-channel';
-        var sender = Math.round(Math.random() * 60535) + 5000;
-
-        io.connect(URL).emit('new-channel', {
-            channel: _channel,
-            sender : sender
-        });
-
-        var socket = io.connect(URL + _channel);
-        socket.channel = _channel;
-        socket.on('connect', function () {
-            if (config.callback) config.callback(socket);
-        });
-
-        socket.send = function (message) {
-            socket.emit('message', {
-                sender: sender,
-                data  : message
-            });
-        };
-
-        socket.on('message', config.onmessage);
-    };
-
-    mainChannel.direction = 'many-to-many';
-
-    var p2pChannels = [];
-
     var nms = ["De Jong", "Jansen", "De Vries", "Van den Berg",
-      "Van Dijk", "Bakker", "Visser", "Smit", "Meijer", "De Boer", "Mulder",
-      "De Groot", "Bos", "Vos", "Peters", "Hendriks", "Van Leeuwen", "Dekker",
-      "Brouwer", "De Wit", "Dijkstra", "Smits", "De Graaf", "Van der Meer"]
+    "Van Dijk", "Bakker", "Visser", "Smit", "Meijer", "De Boer", "Mulder",
+    "De Groot", "Bos", "Vos", "Peters", "Hendriks", "Van Leeuwen", "Dekker",
+    "Brouwer", "De Wit", "Dijkstra", "Smits", "De Graaf", "Van der Meer"]
 
-    mainChannel.userid = nms[Math.floor(Math.random() * nms.length)];
 
-    mainChannel.onopen = function (userid)
-    {
-      // get user filelist
-      $rootScope.$broadcast('rtc-onopen', userid);
-      mainChannel.send('hi!');
-      mainChannel.send({filelistRequest: true});
-    }
+    window.mainChannel = new DataChannel('default-channel',{
+      direction: 'many-to-many',
+      openSignalingChannel: function (config) {
+          var URL = "http://localhost:9002/";
 
-    mainChannel.onleave = function (userid)
-    {
-      console.log(userid);
-      $rootScope.$broadcast('rtc-onleave', userid);
-    }
+          var _channel = config.channel || this.channel || 'default-channel';
+          var sender = Math.round(Math.random() * 60535) + 5000;
 
-    // all incoming messages
-    mainChannel.onmessage = function (message, userid)
-    {
-      // if filelist request
-      if (typeof message == 'object')
+          io.connect(URL).emit('new-channel', {
+              channel: _channel,
+              sender : sender
+          });
+
+          var socket = io.connect(URL + _channel);
+          socket.channel = _channel;
+          socket.on('connect', function () {
+              if (config.callback) config.callback(socket);
+          });
+
+          socket.send = function (message) {
+              socket.emit('message', {
+                  sender: sender,
+                  data  : message
+              });
+          };
+
+          socket.on('message', config.onmessage);
+      },
+      userid: nms[Math.floor(Math.random() * nms.length)],
+      onopen: function (userid)
       {
-        console.log(message);
-        if (typeof message.filelistRequest !== 'undefined'){
-          // send filelist if requested
-          console.log('filelistRequest!');
-          console.log(fileService.ownFiles);
-          mainChannel.channels[userid].send(
-            {filelist:
-              fileService.ownFiles.map(function(x){return x.fullPath;})
-            }
-          );
-        }
-        if (typeof message.filelist !== 'undefined'){
-          mainChannel.channels[userid].filelist = message.filelist;
-        }
-        if (typeof message.requestFile !== 'undefined'){
-
-          // if folder
-          // if single file
-          var file = ownFiles.filter(
-            function(x){return (x.fullPath==message.requestFile)}
-          );
-
-          console.log(file)
-          file[0].file(function(file){mainChannel.channels[userid].send(file)});
-        }
-
-      }
-      else if(typeof message == 'string')
+        // get user filelist
+        $rootScope.$broadcast('rtc-onopen', userid);
+        mainChannel.send({filelistRequest: true});
+      },
+      onleave: function (userid)
       {
-        $rootScope.$broadcast('rtc-onmessage', {msg: message, user: userid});
+        console.log(userid);
+        $rootScope.$broadcast('rtc-onleave', userid);
+      },
+      onmessage: function (message, userid)
+      {
+        // if filelist request
+        if (typeof message == 'object')
+        {
+          console.log(message);
+          if (typeof message.filelistRequest !== 'undefined'){
+            // send filelist if requested
+            console.log('filelistRequest!');
+            console.log(fileService.ownFiles);
+            mainChannel.channels[userid].send(
+              {filelist:
+                fileService.ownFiles.map(function(x){return x.fullPath;})
+              }
+            );
+          }
+          if (typeof message.filelist !== 'undefined'){
+            mainChannel.channels[userid].filelist = message.filelist;
+          }
+          if (typeof message.requestFile !== 'undefined'){
+
+            // if folder
+            // if single file
+            var file = ownFiles.filter(
+              function(x){return (x.fullPath==message.requestFile)}
+            );
+
+            console.log(file)
+            file[0].file(function(file){mainChannel.channels[userid].send(file)});
+          }
+
+        }
+        else if(typeof message == 'string')
+        {
+          $rootScope.$broadcast('rtc-onmessage', {msg: message, user: userid});
+        }
+      },
+      onFileProgress: function (packets)
+      {
+        console.log(packets);
+        $rootScope.$broadcast('rtc-onFileProgress', packets);
+
+        // packets.remaining
+        // packets.sent
+        // packets.received
+        // packets.length
+      },
+      onFileSent: function (file)
+      {
+        console.log(file);
+        $rootScope.$broadcast('rtc-onFileSent', file);
+
+        // file.name
+        // file.size
+      },
+      onFileReceived: function (fileName)
+      {
+        console.log(fileName);
+        $rootScope.$broadcast('rtc-onFileReceived', fileName);
       }
-    }
-
-    mainChannel.onFileProgress = function (packets)
-    {
-      console.log(packets);
-      $rootScope.$broadcast('rtc-onFileProgress', packets);
-
-      // packets.remaining
-      // packets.sent
-      // packets.received
-      // packets.length
-    };
-
-    // on file successfully sent
-    mainChannel.onFileSent = function (file)
-    {
-      console.log(file);
-      $rootScope.$broadcast('rtc-onFileSent', file);
-
-      // file.name
-      // file.size
-    };
-
-    mainChannel.onFileReceived = function (fileName)
-    {
-      console.log(fileName);
-      $rootScope.$broadcast('rtc-onFileReceived', fileName);
-    };
+    });
 
     // Public API here
     return {
--- server/index.js
+++ server/index.js
@@ -2,22 +2,22 @@
 
 var io = require('socket.io').listen(9002);
 io.sockets.on('connection', function (socket) {
-    if (!io.connected) io.connected = true;
+  if (!io.connected) io.connected = true;
 
-    socket.on('new-channel', function (data) {
-        onNewNamespace(socket, data.channel, data.sender);
-    });
+  socket.on('new-channel', function (data) {
+    onNewNamespace(socket, data.channel, data.sender);
+  });
 });
 
 function onNewNamespace(socket, channel, sender) {
-    io.of('/' + channel).on('connection', function (socket) {
-        if (io.isConnected) {
-            io.isConnected = false;
-            socket.emit('connect', true);
-        }
+  io.of('/' + channel).on('connection', function (socket) {
+    if (io.isConnected) {
+      io.isConnected = false;
+      socket.emit('connect', true);
+    }
 
-        socket.on('message', function (data) {
-            if (data.sender == sender) socket.broadcast.emit('message', data.data);
-        });
+    socket.on('message', function (data) {
+      if (data.sender == sender) socket.broadcast.emit('message', data.data);
     });
+  });
 }
